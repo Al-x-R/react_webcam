1c5daefe70593749663bf1f7453b5accd1f592a8
alexander <alexander.roshchin85@gmail.com>
2021-05-06 17:09:29 +0300

    Add receiving video with audio and del video and photo from page

--- src/components/WebCamVideo/WebCamVideo.tsx
+++ src/components/WebCamVideo/WebCamVideo.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import Webcam from "react-webcam";
 
 const WebCamVideo = () => {
@@ -6,14 +6,24 @@ const WebCamVideo = () => {
     const mediaRecorderRef = useRef<any>(null);
     const [capturing, setCapturing] = useState(false);
     const [recordedChunks, setRecordedChunks] = useState([]);
+    const [videoUrl, setVideoUrl] = useState<string | null >(null)
 
-    const handleStartCaptureClick = React.useCallback(() => {
+    const [img, setImg] = useState<string | null>(null)
+    const capture = useCallback(() => {
+        if (webcamRef.current) {
+            const imageSrc = webcamRef.current.getScreenshot()
+            setImg(imageSrc)
+        }
+    }, [webcamRef])
+
+    const handleStartCaptureClick = useCallback(() => {
         if (webcamRef.current) {
             setCapturing(true);
             if (mediaRecorderRef) {
                 if (webcamRef.current.stream) {
                     mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
-                        mimeType: "video/webm"
+                        mimeType: "video/webm",
+                        audioBitsPerSecond : 128000
                     });
                 }
                 if (mediaRecorderRef.current) {
@@ -27,7 +37,7 @@ const WebCamVideo = () => {
         }
     }, [webcamRef, setCapturing, mediaRecorderRef]);
 
-    const handleDataAvailable = React.useCallback(
+    const handleDataAvailable = useCallback(
         ({ data }) => {
             if (data.size > 0) {
                 setRecordedChunks((prev) => prev.concat(data));
@@ -36,43 +46,75 @@ const WebCamVideo = () => {
         [setRecordedChunks]
     );
 
-    const handleStopCaptureClick = React.useCallback(() => {
+    const handleStopCaptureClick = useCallback(() => {
         if (mediaRecorderRef.current) {
             mediaRecorderRef.current.stop();
         }
-
         setCapturing(false);
     }, [mediaRecorderRef, webcamRef, setCapturing]);
 
-    const handleDownload = React.useCallback(() => {
+    useEffect(() => {
         if (recordedChunks.length) {
             const blob = new Blob(recordedChunks, {
                 type: "video/webm"
             });
-            const url = URL.createObjectURL(blob);
-            const a = document.createElement("a");
-            document.body.appendChild(a);
-            // a.style = "display: none";
-            a.href = url;
-            a.download = "react-webcam-stream-capture.webm";
-            a.click();
-            window.URL.revokeObjectURL(url);
-            setRecordedChunks([]);
+            setVideoUrl(URL.createObjectURL(blob))
         }
-    }, [recordedChunks]);
+    }, [recordedChunks])
+    // const handleDownload = useCallback(() => {
+    //     if (recordedChunks.length) {
+    //         const blob = new Blob(recordedChunks, {
+    //             type: "video/webm"
+    //         });
+    //         console.log('blob', blob)
+    //         const url = URL.createObjectURL(blob);
+    //         setVideoUrl(url)
+    //         console.log('url', url.trimRight())
+    //         const a = document.createElement("a");
+    //         document.body.appendChild(a);
+    //         // a.style = "display: none";
+    //         a.href = url;
+    //         a.download = "react-webcam-stream-capture.webm";
+    //         a.click();
+    //         window.URL.revokeObjectURL(url);
+    //         setRecordedChunks([]);
+    //     }
+    // }, [recordedChunks]);
+    const handleClearVideo = useCallback(() => {
+        if (videoUrl) {
+            setVideoUrl(null);
+            window.URL.revokeObjectURL(videoUrl)
+            setRecordedChunks([])
+        }
+    }, [videoUrl])
+
+    const handleClearPhoto = useCallback(() => {
+        if (img) {
+            setImg(null);
+        }
+    }, [img])
 
     return (
         <div>
             <>
-                <Webcam audio={false} ref={webcamRef} />
+                <Webcam audio={true}
+                        ref={webcamRef}
+                        screenshotFormat="image/jpeg"
+                        />
                 {capturing ? (
                     <button onClick={handleStopCaptureClick}>Stop Capture</button>
                 ) : (
                     <button onClick={handleStartCaptureClick}>Start Capture</button>
                 )}
-                {recordedChunks.length > 0 && (
-                    <button onClick={handleDownload}>Download</button>
-                )}
+                <button onClick={capture}>Capture photo</button>
+                {img && <div>
+                    <img src={img} alt=""/>
+                    <button onClick={handleClearPhoto}>Clear</button>
+                </div>}
+                {videoUrl && <div>
+                    <video controls={true} src={videoUrl}/>
+                    <button onClick={handleClearVideo}>Clear</button>
+                </div> }
             </>
         </div>
     );
